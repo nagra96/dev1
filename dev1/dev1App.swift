8f01084d83d0a13b0055daee5fd48702e3edd937
nagra <god>
2025-07-19 21:40:17 -0600

    Initial Commit

---
+++ dev1/dev1App.swift
@@ -0,0 +1,17 @@
+//
+//  dev1App.swift
+//  dev1
+//
+//  Created by RAJVIR KAUR on 2025-07-19.
+//
+
+import SwiftUI
+
+@main
+struct dev1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
